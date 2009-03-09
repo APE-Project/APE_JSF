cc2e362db6b6b15ac0fc55bf69883ec783c547a6
efyx <n.trani@weelya.com>
2009-03-09 18:17:01 +0000

    Adding new object Ape_client to interact with ape core :
    - Load ape core
    - Intercept event
    - Add event
    
    Two version of this object is available. Mootools version, Pure js version

--- ape/client_js.js
+++ ape/client_js.js
@@ -1,9 +1,68 @@
-function ape_client(core){
+function Ape_client(core){
 	this._core = core;
 }
-ape_client.prototype.fire_event = function(type,args,delay){
+Ape_client.prototype.fire_event = function(type,args,delay){
 	this._core.fireEvent(type,args,delay);
 }
-ape_client.prototype.add_event = function(type,fn,internal){
-	this._core.addEvent(type,args,delay);
+Ape_client.prototype.add_event = function(type,fn,internal){
+	this._core.addEvent(type,fn,internal);
+}
+Ape_client.prototype.ape_cookie = function (name,remove) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0){
+			return c.substring(nameEQ.length,c.length);
+		}
+	}
+	return null;
+}
+
+Ape_client.prototype.load = function(config){
+	this.ape_config = config;
+	config.init_ape = config.init_ape || true;
+	var tmp = eval('('+unescape(this.ape_cookie('Ape_cookie'))+')');
+	config.frequency = config.frequency || 0;
+	if(tmp){
+		config.frequency = parseInt(tmp.frequency);
+	}
+	var restore = this.ape_cookie('Ape_restore')
+	if(restore){
+		config.frequency = restore;
+	}
+	config.init = function(core){
+		this._core = core;
+	}.bind(this);
+	document.domain = config.domain;
+	var frame = document.createElement('iframe');
+	frame.setAttribute('id','ape_frequency');
+	frame.style.display = 'none';
+	frame.style.position = 'absolute';
+	frame.style.left = '-300px';
+	frame.style.top = '-300px';
+	document.body.appendChild(frame);
+	frame.setAttribute('src','http://'+config.frequency+'.'+config.server+'?q&script&'+config.scripts.join('&')+'&ac');
+}
+if(Function.prototype.bind==null){
+	Function.prototype.bind = function(bind,args){
+		return this.create({bind: bind,arguments:args});
+	}
+}
+if(Function.prototype.create==null){
+	Function.prototype.create = function(options){
+			var self = this;
+			options = options || {};
+			return function(){
+				var args = options.arguments || arguments;
+				if(args && !args.length){
+					args = [args];
+				}
+				var returns = function(){
+					return self.apply(options.bind || null, args);
+				};
+				return returns();
+			};
+	}
 }
--- ape/client_moo.js
+++ ape/client_moo.js
@@ -4,5 +4,32 @@ var Ape_client = new Class({
 	},
 	add_event: function(type,fn,internal){
 		this._core.addEvent(type,fn.bind(this),internal);
+	},
+	load: function(config){
+		this.ape_config = config;
+		config.init_ape = config.init_ape || true;
+		var tmp = JSON.decode(Cookie.read('Ape_cookie'));
+		config.frequency = config.frequency || 0;
+		if(tmp){
+			config.frequency = tmp.frequency.toInt();
+		}
+		var restore = Cookie.read('Ape_restore')
+		if(restore){
+			config.frequency = restore;
+		}
+		config.init = function(core){
+			this._core = core;
+		}.bind(this);
+		document.domain = config.domain;
+		new Element('iframe',{
+			'id':'ape_'+config.identifier,
+			'styles':{
+				'display':'none',
+				'position':'absolute',
+				'left':'-300px',
+				'top':'-300px',
+			},
+			'src':'http://'+config.frequency+'.'+config.server+'?q&script&'+config.scripts.join('&')+'&ac'
+		}).inject(document.body);
 	}
 });
