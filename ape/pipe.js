46d9179d81d1b1854660d276d8d24d28ece0c82c
efyx <n.trani@weelya.com>
2009-05-02 16:46:20 +0000

    Opera fix

--- ape/pipe.js
+++ ape/pipe.js
@@ -11,12 +11,14 @@ var Ape_pipe  = new Class({
 		this.request('SEND',[this.get_pubid(), escape(data)]);
 	},
 	request: function(raw, param, sessid, options){
-		var tmp = {'event': false}
+		var tmp = {'event': false}, event_param = param;
+
 		this._core.request(raw, param, sessid, options ? $extend(tmp, options) : tmp);
 		if (!$type(sessid) || sessid) {
-			param.unshift(this._core.get_sessid());
+			//I know i would be better to use param.unshift but when i did it opera get some trouble (try it yourself if you want to know what happend)
+			event_param = [this._core.get_sessid()].combine(param);
 		}
-		this.fire_event('cmd_' + raw.toLowerCase(), [this].combine(param));
+		this.fire_event('cmd_' + raw.toLowerCase(), [this].combine(event_param));
 	},
 	get_pubid: function(){
 		return this.pipe.pubid;
