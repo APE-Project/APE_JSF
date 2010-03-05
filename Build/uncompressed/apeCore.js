bd2a720df934b95699a46f0c1110594683f7c46f
efyx <n.trani@weelya.com>
2010-03-05 17:48:34 +0100

    Fix bug with IE8 and hidden pipe

--- Build/uncompressed/apeCore.js
+++ Build/uncompressed/apeCore.js
@@ -2286,7 +2286,7 @@ APE.PipeMulti = new Class({
 	},
 
 	rawLeft: function(raw, pipe) {
-		if (pipe.name[0] != '*') this.delUser(raw.data.user.pubid);
+		if (pipe.name.charAt(0) != '*') this.delUser(raw.data.user.pubid);
 		if (raw.data.user.pubid == this.ape.user.pubid) this.ape.delPipe(pipe.pipe.pubid);
 	},
 
